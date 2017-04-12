61a621707a7cfc7f76524c63f8eb7317e32bff53
NoeCutz <noecutz03@gmail.com>
2017-04-12 15:22:51 -0500

    Tratando de hacer funcionales las opciones de la barra de navegación

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>pagina principal</title>
+  </head>
+  <body>
+    <section id="articulos">
+    </section>
+  </body>
+</html>
